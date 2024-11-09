3ca279d01dd8d40a30d41b8bb482119a483e5d23
Mackenzie Sheridan <mackenzielsheridan@gmail.com>
2024-11-08 20:18:45 -0600

    updating tf

---
+++ terraform/app/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "Name of the AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_role_arn" {
+  description = "Name of the AWS role to assume when running terraform"
+  type        = string
+  default     = "arn:aws:iam::992382748278:role/terraform-role"
+}
+
+variable "lambda_recipient_email" {
+  description = "Recipient email for SES"
+  type        = string
+  default     = "mackenzielsheridan+recipient@gmail.com"
+}
+
+variable "lambda_sender_email" {
+  description = "Sender email for SES"
+  type        = string
+  default     = "mackenzielsheridan+sender@gmail.com"
+}
+
+variable "sqs_queue_name" {
+  description = "Queue name that consumes from reddit"
+  type        = string
+  default     = "reddit-queue"
+}
+
+variable "reddit_sns_topic" {
+  description = "SNS topic name that consumes from reddit queue"
+  type        = string
+  default     = "reddit-sns-topic"
+}
